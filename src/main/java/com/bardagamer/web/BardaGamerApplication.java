9815f0381d58a5526d6aea9a568c1f128d61365a
kakashi-p87 <acmilanp7@hotmail.com>
2018-08-16 01:30:30 -0500

    adding project

---
+++ src/main/java/com/bardagamer/web/BardaGamerApplication.java
@@ -0,0 +1,12 @@
+package com.bardagamer.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BardaGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BardaGamerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bardagamer/web/BardaGamerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bardagamer.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BardaGamerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
